278007ab9dd760d6dc1da8663ba87e90c317cfa6
jneblina <janneblina7@gmail.com>
2023-02-08 11:43:42 -0800

    Agregue my_card

---
+++ my_card/lib/home_page.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key}); //Constructor
+
+  @override //Clase abstracta que se sobreescribe
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      body: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const CircleAvatar(
+                  radius: 100,
+                  backgroundImage: AssetImage('assets/images/goku.jpg'),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  "Neblina Villegas Jan Marlon",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.deepPurple,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  "Flutter Developer",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.deepPurple,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 20,
+                  width: 200,
+                  child: Divider(color: Colors.white, thickness: 2),
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 10.0),
+                  color: Colors.white,
+                  width: 350.0,
+                  height: 40,
+                  child: Row(children: const [
+                    Padding(padding: EdgeInsets.all(10)),
+                    Icon(
+                      Icons.phone,
+                      color: Colors.purple,
+                    ),
+                    SizedBox(
+                      width: 40,
+                    ),
+                    Text(
+                      "+52 635-113-43-36",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ]),
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 10.0),
+                  color: Colors.white,
+                  width: 350.0,
+                  height: 40,
+                  child: Row(children: const [
+                    Padding(padding: EdgeInsets.all(10)),
+                    Icon(
+                      Icons.email,
+                      color: Colors.purple,
+                    ),
+                    SizedBox(
+                      width: 40,
+                    ),
+                    Text(
+                      "janneblina7@gmail.com",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ]),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
